eb2604addad9b46b80643e912ea82c0be44ccb54
Arthur <arthurcorria0109@gmail.com>
2026-04-04 10:17:56 -0300

    feat: add project

---
+++ src/main/java/br/edu/cesmac/odontaval/OdontavalApplication.java
@@ -0,0 +1,13 @@
+package br.edu.cesmac.odontaval;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdontavalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OdontavalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/cesmac/odontaval/OdontavalApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.cesmac.odontaval;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdontavalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
