70d565ba51ec7bb5930bf121396ab4d9ca5dc967
Arthur <arthurcorria0109@gmail.com>
2026-04-04 12:42:40 -0300

    fix(V2__create_table_users.sql): fix 'specialism' foreign key column name

--- src/main/resources/db/migration/V2__create_table_users.sql
+++ src/main/resources/db/migration/V2__create_table_users.sql
@@ -4,7 +4,7 @@ CREATE TABLE users
     name       VARCHAR(100) NOT NULL,
     email      VARCHAR(150) NOT NULL UNIQUE,
     password   VARCHAR(255) NOT NULL,
-    specialism INTEGER REFERENCES specialisms (id),
+    specialism_id INTEGER REFERENCES specialisms (id),
 
     created_at TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
     created_by INTEGER,
